2641adb3f0ca99c50239747674131a635a4e47c9
neominal.solutions <neominal.solutions@gmail.com>
2024-04-08 16:39:59 +0300

    ClassComponentsDemo

--- src/components/demo/ClassComponentsDemo.jsx
+++ src/components/demo/ClassComponentsDemo.jsx
@@ -27,6 +27,7 @@ export default class ClassComponentsDemo extends Component {
 		});
 	};
 
+	// Doğru kabul edilen kullanımı
 	increase() {
 		this.setState({ counter: this.state.counter + 1 });
 		// this.counter++;
